050ce3ec6befce4465a9662cf53c6078b6abe571
KikA-DyDx <edacocomputers@gmail.com>
2025-05-19 07:27:21 +0400

    Allowed Hosts

--- courier_site/settings.py
+++ courier_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-3zk5yxp^fpo42$yaye@p*q*3f+=%lq^qt%pnozjtms&0u*zc(e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['essexglobalexpress.onrender.com']
 
 
 # Application definition
